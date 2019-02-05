378bc55ee3407caf1256a40bcbfb3980bc5c8e67
Mitch Sullivan <sullivan.mitch@gmail.com>
2019-02-05 01:09:16 -0330

    Clean up run_simulation.py

--- run_simulation.py
+++ run_simulation.py
@@ -16,11 +16,17 @@ D_GAIN = 0.1
 ### Process parameters
 SETPOINT = 1.0
 INITIAL_PROCESS_VARIABLE = 0
-### Actuation parameters  
-MAX_RANDOM_ERROR = 0.001
-OUTPUT_ACTUATION_FACTOR = 0.01
 ### Simulation parameters
 TOTAL_DURATION_S = 5
+OUTPUT_INTERVAL_S = 0.001
+# Define function used to transform controller output to a process variable value
+# - Must have signature def ACTUATE(process_variable, controller_output)
+# - Must return an updated value of the process variable
+###
+def ACTUATE(process_variable: float, controller_output: float)  -> float:
+    output = 0.01 * controller_output
+    random_fluctuation = 0.001 * random.random()
+    return (process_variable + output) * (1.0 + random_fluctuation)
 ####
 
 
@@ -28,31 +34,28 @@ def main():
     data = run_simulation(
         PIDController(P_GAIN, I_GAIN, D_GAIN, SETPOINT),
         INITIAL_PROCESS_VARIABLE,
-        lambda pv, co: actuate(pv, co, MAX_RANDOM_ERROR, OUTPUT_ACTUATION_FACTOR),
+        ACTUATE,
         TOTAL_DURATION_S
     )
     
     plot_data(data, SETPOINT, INITIAL_PROCESS_VARIABLE)
-
-    
-def actuate(process_variable, controller_output, max_random_error, output_actuation_factor):
-    output_actuation = process_variable + output_actuation_factor * controller_output
-    random_fluctuation = output_actuation * max_random_error * random.random()
-    return output_actuation + random_fluctuation
     
 
-def run_simulation(controller, process_variable, actuator_function, total_duration_s):
+def run_simulation(controller, initial_process_variable, actuator_function, total_duration_s):
     total_duration = timedelta(seconds=total_duration_s)
-    start_time = datetime.now()
-    data = [(0.0, process_variable, 0.0, controller.setpoint - process_variable)]
+    data = [(0.0, initial_process_variable, 0.0, controller.setpoint - initial_process_variable)]
+    process_variable = initial_process_variable
     time_elapsed = timedelta(seconds=0)
+    start_time = datetime.now()
+
     while time_elapsed < total_duration:
         current_time = datetime.now()
         time_elapsed = current_time - start_time
         controller_output, error_value = controller.get_output(process_variable)
         process_variable = actuator_function(process_variable, controller_output)
         data.append((time_elapsed.total_seconds(), process_variable, controller_output, error_value))
-        time.sleep(0.001) # 1 ms
+        time.sleep(OUTPUT_INTERVAL_S)
+
     return data
 
 
